f2505c6b4d48cbdc2162a6664b86d5e0315be8fd
Jim Nelson <jim@midnightfreddie.com>
2016-09-22 21:15:27 -0500

    Trying to iterate

--- main.go
+++ main.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
 func main() {
-	db, _ := leveldb.OpenFile("db/test.ldb", nil)
+	db, err := leveldb.OpenFile("db", nil)
+	if err != nil {
+		panic("error")
+	}
 	defer db.Close()
+
+	iter := db.NewIterator(nil, nil)
+
+	for i := 1; i < 1000; iter.Next() {
+		// Remember that the contents of the returned slice should not be modified, and
+		// only valid until the next call to Next.
+		key := iter.Key()
+		value := iter.Value()
+		fmt.Println(key)
+		fmt.Println(value)
+		i++
+	}
+	iter.Release()
+	err = iter.Error()
 }
