b7a719255239a2f5c70031f7371be936ca62f33f
Jim Nelson <jim@midnightfreddie.com>
2016-09-23 16:20:10 -0500

    failing with this nbt reader; about to try another

--- main.go
+++ main.go
@@ -3,16 +3,25 @@ package main
 // Note: In my $GOPATH/src I have github.com/midnightfreddie/goleveldb/leveldb (addzlib branch) in place of github.com/syndtr/goleveldb/leveldb
 //   This adds zlib decompression to the reader as compression type 2 which is needed to read MCPE ldb files
 import (
+	"bytes"
 	"fmt"
 
+	"github.com/quag/mcobj/nbt"
 	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/opt"
 )
 
 // Note: Open the .mcworld file as a zip--rename it to .mcworld.zip if needed--then copy the db folder
 //  to the folder where you'll be running this program
 
 func main() {
-	db, err := leveldb.OpenFile("db", nil)
+	// db, err := leveldb.OpenFile("db", nil)
+	// Setting readOnly to true
+	//   now thinking I can read directly from the zip file, perhaps
+	o := &opt.Options{
+		ReadOnly: true,
+	}
+	db, err := leveldb.OpenFile("db", o)
 	if err != nil {
 		panic("error")
 	}
@@ -23,6 +32,11 @@ func main() {
 		panic("error")
 	}
 	fmt.Println(string(player[:]))
+	nbtr := bytes.NewReader(player)
+	mynbt := nbt.NewReader(nbtr)
+	// out, _ := nbt.Parse(nbtr)
+	// fmt.Println(json.Marshal(out))
+	fmt.Println(mynbt.ReadStruct())
 
 	// iterate and print the first 10 key/value pairs
 	iter := db.NewIterator(nil, nil)
