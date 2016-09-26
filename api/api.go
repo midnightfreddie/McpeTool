9f449951548f91ee4922b916d438af3ac2e158e6
Jim Nelson <jim@midnightfreddie.com>
2016-09-26 14:37:25 -0500

    api returning keys in JSON/base64 format

--- api/api.go
+++ api/api.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,33 +13,33 @@ import (
 func Server(db *leveldb.DB) error {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		keylist := [][]byte{}
 		iter := db.NewIterator(nil, nil)
 		for iter.Next() {
 			key := iter.Key()
-			switch {
-			case len(key) == 9:
-				switch key[8] {
-				case 0x30, 0x31, 0x32, 0x76:
-					fmt.Fprintln(w, key)
-				default:
-					fmt.Fprintln(w, string(key[:]))
-				}
-			case len(key) == 13:
-				switch key[12] {
-				case 0x30, 0x31, 0x32, 0x76:
-					fmt.Fprintln(w, key)
-				default:
-					fmt.Fprintln(w, string(key[:]))
-				}
-			default:
-				fmt.Fprintln(w, string(key[:]))
-			}
+			tmp := make([]byte, len(key))
+			copy(tmp, key)
+			keylist = append(keylist, tmp)
+			// outJson, err := json.Marshal(key)
+			// json.NewEncoder(w).Encode(key)
+			// if err != nil {
+			// 	panic(err.Error())
+			// }
+			// fmt.Println(string(outJson[:]))
 		}
 		iter.Release()
 		err := iter.Error()
 		if err != nil {
 			panic(err.Error())
 		}
+		// fmt.Fprintf(w, "%v\n\n", keylist)
+		outJson, err := json.MarshalIndent(keylist, "", "  ")
+		// outJson, err := json.Marshal(keylist)
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Fprintln(w, string(outJson[:]))
+		// json.NewEncoder(w).Encode(keylist)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
