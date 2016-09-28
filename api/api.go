7f0e7f6f9a839bf941f698f72c86e26abb0f4b01
Jim Nelson <jim@midnightfreddie.com>
2016-09-27 23:42:24 -0500

    Implemented master JSON response object

--- api/api.go
+++ api/api.go
@@ -10,52 +10,91 @@ import (
 	"github.com/midnightfreddie/McpeTool/world"
 )
 
+var apiVersion = "0.0"
+
+// Response is the default JSON response object
+type Response struct {
+	key        []byte
+	keys       [][]byte
+	data       []byte
+	ApiVersion string `json:"apiVersion"`
+	Context    string `json:"context,omitempty"`
+	Keys       []Key  `json:"keys,omitempty"`
+	KeyString  string `json:"keyString,omitempty"`
+	Base64Key  string `json:"base64Key,omitempty"`
+	Key        []int  `json:"key,omitempty"`
+	Base64Data string `json:"base64Data,omitempty"`
+}
+
+func NewResponse() *Response {
+	return &Response{ApiVersion: apiVersion}
+}
+
+// Fill is used to conver the raw data to JSON-friendly data before returning to client
+func (o *Response) Fill() {
+	o.KeyString, o.Base64Key, o.Key = convertKey(o.key)
+	o.Keys = make([]Key, len(o.keys))
+	for i := range o.Keys {
+		o.Keys[i].KeyString, o.Keys[i].Base64Key, o.Keys[i].Key = convertKey(o.keys[i])
+	}
+}
+
 type Key struct {
+	key       []byte
 	KeyString string `json:"keyString,omitempty"`
 	Base64Key string `json:"base64Key"`
 	Key       []int  `json:"key"`
 }
 
-// SetKey is used to set the base64 and byte array versions of the key and ensure consistency
-func (k *Key) SetKey(key []byte) {
+func convertKey(k []byte) (keyString, base64Key string, intArray []int) {
 	// json.Marshall will base64-encode byte arrays instead of making a JSON array, so making an array of ints to get desired behavior in JSON output
-	k.Key = make([]int, len(key))
+	intArray = make([]int, len(k))
 	allAscii := true
-	for i := range key {
-		k.Key[i] = int(key[i])
-		if key[i] < 0x20 || key[i] > 0x7e {
+	for i := range k {
+		intArray[i] = int(k[i])
+		if k[i] < 0x20 || k[i] > 0x7e {
 			allAscii = false
 		}
 	}
 	if allAscii {
-		k.KeyString = string(key[:])
+		keyString = string(k[:])
 	}
-	k.Base64Key = base64.StdEncoding.EncodeToString(key)
+	base64Key = base64.StdEncoding.EncodeToString(k)
+	return
 }
 
-// KeyList is the structure used for JSON replies to key list requests
-type KeyList struct {
-	Keys []Key `json:"keys"`
+// Fill is used to set the base64 and int array versions of the key
+func (k *Key) Fill() {
+	k.KeyString, k.Base64Key, k.Key = convertKey(k.key)
 }
 
-// SetKeys is used to populate an array of Keys
-func (k *KeyList) SetKeys(inKeyList [][]byte) {
-	outKeyList := make([]Key, len(inKeyList))
-	for i := 0; i < len(inKeyList); i++ {
-		outKeyList[i].SetKey(inKeyList[i])
-	}
-	k.Keys = append(k.Keys, outKeyList...)
-}
+// // KeyList is the structure used for JSON replies to key list requests
+// type KeyList struct {
+// 	Keys []Key `json:"keys"`
+// }
+
+// // SetKeys is used to populate an array of Keys
+// func (k *KeyList) SetKeys(inKeyList [][]byte) {
+// 	outKeyList := make([]Key, len(inKeyList))
+// 	for i := 0; i < len(inKeyList); i++ {
+// 		outKeyList[i].SetKey(inKeyList[i])
+// 	}
+// 	k.Keys = append(k.Keys, outKeyList...)
+// }
 
 // Server is the http REST API server
 func Server(world *world.World) error {
 	http.HandleFunc("/api/v1/db/", func(w http.ResponseWriter, r *http.Request) {
-		keylist, err := world.GetKeys()
+		var err error
+		outData := NewResponse()
+		outData.keys, err = world.GetKeys()
 		if err != nil {
 			panic(err.Error())
 		}
-		outData := KeyList{}
-		outData.SetKeys(keylist)
+		outData.Fill()
+		// outData := KeyList{}
+		// outData.SetKeys(keylist)
+
 		outJson, err := json.MarshalIndent(outData, "", "  ")
 		// outJson, err := json.Marshal(keylist)
 		if err != nil {
