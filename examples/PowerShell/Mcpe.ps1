3af2e8698f5639822b8f85da21db21d67f13a671
Jim Nelson <jim@midnightfreddie.com>
2016-09-28 17:40:36 -0500

    added examples folder

---
+++ examples/PowerShell/Mcpe.ps1
@@ -0,0 +1,33 @@
+﻿[cmdletbinding()]
+param (
+    [string]$EndPoint = "http://localhost:8080",
+    [string]$ApiRoot = "/api/v1/db"
+)
+
+function ConvertTo-Base64 {
+    param ( [byte[]]$ByteArray )
+    [System.Convert]::ToBase64String($ByteArray)
+}
+
+function ConvertFrom-Base64 {
+    param ( [string]$Base64String )
+    [System.Text.Encoding]::UTF8.GetString(
+        [System.Convert]::FromBase64String($Base64String)
+    )
+}
+
+$Data = [System.Text.Encoding]::UTF8.GetBytes("Hello")
+$HexKey = "010203"
+$Uri = $EndPoint + $ApiRoot + "/" + $HexKey
+$Body = New-Object psobject -Property @{
+    base64Data = ConvertTo-Base64 $Data
+} | ConvertTo-Json
+
+$DeleteResult = Invoke-WebRequest -Uri $Uri -Method Delete
+
+$PutResult = Invoke-WebRequest -Uri $Uri -Method Put -Body $Body
+
+Remove-Variable GetResult
+$GetResult = Invoke-WebRequest -Uri $Uri
+
+$GetResult
