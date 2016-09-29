58c06f65801f12c551b7575f34c4b2e1b2362702
Jim Nelson <jim@midnightfreddie.com>
2016-09-29 18:46:30 -0500

    slow progress reading chunks

--- anvil2pe/main.go
+++ anvil2pe/main.go
@@ -1,5 +1,4 @@
-// Taking some code snippets from https://github.com/jteeuwen/mctools/blob/master/anvil for this file
-// Also using its modules
+// Using modules from https://github.com/jteeuwen/mctools/blob/master/anvil for this file
 // It is BSD "1-clause" licensed. Pretty sure it and MIT are compatible, but will need to ensure I comply with all notifications
 
 package main
@@ -20,5 +19,17 @@ func main() {
 		panic(err.Error())
 	}
 	fmt.Println(region.X, region.Z)
-	fmt.Printf("%v\n", region.Chunks())
+	fmt.Printf("%v\n", region.ChunkLen())
+	chunks := region.Chunks()
+	anvilChunk := anvil.Chunk{}
+	// for i := range chunks {
+	for i := 0; i < 1; i++ {
+		success := region.ReadChunk(chunks[i][0], chunks[i][1], &anvilChunk)
+		if !success {
+			continue
+		}
+		section := anvilChunk.Section(0, false)
+		fmt.Println(section)
+	}
+
 }
