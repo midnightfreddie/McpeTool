4e7b32959643459da86866a95979e8cfb69871a2
Jim Nelson <jim@midnightfreddie.com>
2016-09-30 00:13:11 -0500

    successfully copying entire region blocks

--- anvil2pe/main.go
+++ anvil2pe/main.go
@@ -7,10 +7,8 @@
 package main
 
 import (
-	"encoding/base64"
-	"fmt"
-
 	"github.com/jteeuwen/mctools/anvil"
+	"github.com/midnightfreddie/McpeTool/world"
 )
 
 // does not validate for sanity
@@ -27,46 +25,67 @@ func peOffset(x, y, z int) (offset int) {
 	return
 }
 
+func int2bytes(i int) [4]byte {
+	return [4]byte{byte(i % 256), byte(i >> 8 % 256), byte(i >> 16 % 256), byte(i >> 24 % 256)}
+}
+
 func main() {
 	region, err := anvil.LoadRegion("region/r.-1.0.mca")
 	if err != nil {
 		panic(err.Error())
 	}
+	world, err := world.OpenWorld("C:\\Users\\Jim.AD\\AppData\\Local\\Packages\\Microsoft.MinecraftUWP_8wekyb3d8bbwe\\LocalState\\games\\com.mojang\\minecraftWorlds\\cNUBAJvzpQA=")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer world.Close()
 	// fmt.Println(region.X, region.Z)
 	// fmt.Printf("%v\n", region.ChunkLen())
 	chunks := region.Chunks()
 	anvilChunk := anvil.Chunk{}
-	// for i := range chunks {
-	for i := 1; i < 2; i++ {
-		success := region.ReadChunk(chunks[i][0], chunks[i][1], &anvilChunk)
+	for chunkIdx := range chunks {
+		// for i := 1; i < 2; i++ {
+		success := region.ReadChunk(chunks[chunkIdx][0], chunks[chunkIdx][1], &anvilChunk)
 		if !success {
 			continue
 		}
-	}
-	peChunk := make([]byte, 83200)
-	// Full brighness for blocks instead of full dark
-	for i := 0xc000; i < 0x14000; i++ {
-		peChunk[i] = 0xff
-	}
-	// Grass color
-	grassColor := [...]byte{0x7, 0x7d, 0xac, 0x6c}
-	for i := 0x14100; i < 0x14500; i += 4 {
-		copy(peChunk[i:], grassColor[:])
-	}
-	for sIdx := range anvilChunk.Sections {
-		section := anvilChunk.Sections[sIdx]
-		if section.Y > 7 {
-			continue
+		peChunk := make([]byte, 83200)
+		// Full brighness for blocks instead of full dark
+		for i := 0xc000; i < 0x14000; i++ {
+			peChunk[i] = 0xff
 		}
-		yBase := 16 * int(section.Y)
-		for y := yBase; y < yBase+16; y++ {
-			for x := 0; x < 16; x++ {
-				for z := 0; z < 16; z++ {
-					_, aIdx := anvilOffset(x, y, z)
-					peChunk[peOffset(x, y, z)] = section.Blocks[aIdx]
+		// Grass color
+		grassColor := [...]byte{0x7, 0x7d, 0xac, 0x6c}
+		for i := 0x14100; i < 0x14500; i += 4 {
+			copy(peChunk[i:], grassColor[:])
+		}
+		for sIdx := range anvilChunk.Sections {
+			section := anvilChunk.Sections[sIdx]
+			if section.Y > 7 {
+				continue
+			}
+			yBase := 16 * int(section.Y)
+			for y := yBase; y < yBase+16; y++ {
+				for x := 0; x < 16; x++ {
+					for z := 0; z < 16; z++ {
+						_, aIdx := anvilOffset(x, y, z)
+						peChunk[peOffset(x, y, z)] = section.Blocks[aIdx]
+					}
 				}
 			}
 		}
+		// fmt.Println(base64.StdEncoding.EncodeToString(peChunk))
+		// fmt.Println(int2bytes(chunks[chunkIdx][0]), int2bytes(chunks[chunkIdx][1]))
+		peKey := make([]byte, 9)
+		tmp := int2bytes(chunks[chunkIdx][0] - 9)
+		copy(peKey, tmp[:])
+		tmp = int2bytes(chunks[chunkIdx][1] - 12)
+		copy(peKey[4:], tmp[:])
+		peKey[8] = 0x30
+		// fmt.Println(peKey)
+		err = world.Put(peKey, peChunk)
+		if err != nil {
+			panic(err.Error())
+		}
 	}
-	fmt.Println(base64.StdEncoding.EncodeToString(peChunk))
 }
---
+++ anvil2pe/readme.md
@@ -0,0 +1,3 @@
+# Not ready for use
+
+I'm in early development of anvil2pe and currently using hard-coded file locations. McpeTool is usable, though.
