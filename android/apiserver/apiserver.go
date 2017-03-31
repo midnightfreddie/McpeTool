f835462581b2b7e32b8f935e307c1d459477281b
Jim Nelson <jim@midnightfreddie.com>
2017-03-30 22:52:42 -0500

    works reading world folder names Problem that was tripping me up was err.Error() being invalid because err was nil

--- android/apiserver/apiserver.go
+++ android/apiserver/apiserver.go
@@ -1,41 +1,66 @@
 package apiserver
 
 import (
-	"github.com/midnightfreddie/McpeTool/api"
-	"github.com/midnightfreddie/McpeTool/world"
+	"io"
+	"io/ioutil"
+	"net/http"
 )
 
 // func StartApiServer(path string) {
 func StartApiServer() {
-	path := `/storage/emulated/0/games/com.mojang/minecraftWorldsh4wKANYDAQA\=`
-	world, err := world.OpenWorld(path)
+	files, err := ioutil.ReadDir(`/storage/emulated/0/games/com.mojang/minecraftWorlds`)
 	if err != nil {
-		// go func() {
-		// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// 		// files, err := ioutil.ReadDir(path + "/db")
-		// 		files, err := ioutil.ReadDir(`/storage/emulated/0/games/com.mojang/minecraftWorlds`)
-		// 		if err != nil {
-		// 			http.Error(w, err.Error(), 500)
-		// 			return
-		// 		}
-		// 		io.WriteString(w, "<html><head><title>HelloWorld</title></head><body><h1 style=\"color: red;\">Hello world!</h1><ul>")
-		// 		for _, file := range files {
-		// 			// fmt.Println(file.Name())
-		// 			io.WriteString(w, "<li>"+string(file.Name())+"</li>")
-		// 		}
-		// 		io.WriteString(w, err.Error())
-
-		// 		io.WriteString(w, "</ul></body></html>")
-		// 	})
-		// 	http.ListenAndServe(":8080", nil)
-		// }()
+		go func() {
+			http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+				io.WriteString(w, "<html><head><title>ApiError</title></head><body><h1 style=\"color: red;\">API Error</h1><div>"+err.Error()+"</div></body></html>")
+			})
+			http.ListenAndServe(":8080", nil)
+		}()
 		return
 	}
-	defer world.Close()
 	go func() {
-		err = api.Server(&world)
-		if err != nil {
-			return
-		}
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			// files, err := ioutil.ReadDir(path + "/db")
+			io.WriteString(w, "<html><head><title>HelloWorld</title></head><body><h1 style=\"color: red;\">Hello world!</h1><ul>")
+			for _, file := range files {
+				// fmt.Println(file.Name())
+				io.WriteString(w, "<li>"+string(file.Name())+"</li>")
+			}
+			// io.WriteString(w, err.Error())
+
+			io.WriteString(w, "</ul></body></html>")
+		})
+		http.ListenAndServe(":8080", nil)
 	}()
+	// path := `/storage/emulated/0/games/com.mojang/minecraftWorldsh4wKANYDAQA\=`
+	// world, err := world.OpenWorld(path)
+	// if err != nil {
+	// 	go func() {
+	// 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 			// files, err := ioutil.ReadDir(path + "/db")
+	// 			files, err := ioutil.ReadDir(`/storage/emulated/0/games/com.mojang/minecraftWorlds`)
+	// 			if err != nil {
+	// 				http.Error(w, err.Error(), 500)
+	// 				return
+	// 			}
+	// 			io.WriteString(w, "<html><head><title>HelloWorld</title></head><body><h1 style=\"color: red;\">Hello world!</h1><ul>")
+	// 			for _, file := range files {
+	// 				// fmt.Println(file.Name())
+	// 				io.WriteString(w, "<li>"+string(file.Name())+"</li>")
+	// 			}
+	// 			io.WriteString(w, err.Error())
+
+	// 			io.WriteString(w, "</ul></body></html>")
+	// 		})
+	// 		http.ListenAndServe(":8080", nil)
+	// 	}()
+	// 	return
+	// }
+	// defer world.Close()
+	// go func() {
+	// 	err = api.Server(&world)
+	// 	if err != nil {
+	// 		return
+	// 	}
+	// }()
 }
