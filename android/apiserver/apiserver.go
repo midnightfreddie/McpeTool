bb248311a5c305732a1b600fb2b6d99754aa9e9d
Jim Nelson <jim@midnightfreddie.com>
2017-03-30 23:10:33 -0500

    still works reading dir in goroutine stepwise commits while troubleshooting

--- android/apiserver/apiserver.go
+++ android/apiserver/apiserver.go
@@ -8,26 +8,24 @@ import (
 
 // func StartApiServer(path string) {
 func StartApiServer() {
-	files, err := ioutil.ReadDir(`/storage/emulated/0/games/com.mojang/minecraftWorlds`)
-	if err != nil {
-		go func() {
-			http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-				io.WriteString(w, "<html><head><title>ApiError</title></head><body><h1 style=\"color: red;\">API Error</h1><div>"+err.Error()+"</div></body></html>")
-			})
-			http.ListenAndServe(":8080", nil)
-		}()
-		return
-	}
 	go func() {
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			// files, err := ioutil.ReadDir(path + "/db")
+			files, err := ioutil.ReadDir(`/storage/emulated/0/games/com.mojang/minecraftWorlds`)
+			if err != nil {
+				go func() {
+					http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+						io.WriteString(w, "<html><head><title>ApiError</title></head><body><h1 style=\"color: red;\">API Error</h1><div>"+err.Error()+"</div></body></html>")
+					})
+					http.ListenAndServe(":8080", nil)
+				}()
+				return
+			}
 			io.WriteString(w, "<html><head><title>HelloWorld</title></head><body><h1 style=\"color: red;\">Hello world!</h1><ul>")
 			for _, file := range files {
 				// fmt.Println(file.Name())
 				io.WriteString(w, "<li>"+string(file.Name())+"</li>")
 			}
-			// io.WriteString(w, err.Error())
-
 			io.WriteString(w, "</ul></body></html>")
 		})
 		http.ListenAndServe(":8080", nil)
