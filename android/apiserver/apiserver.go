f2308d182b7c3e9213660b72220fdb5e6d009d27
Jim Nelson <jim@midnightfreddie.com>
2017-03-31 00:39:03 -0500

    WORKING MAGIC FIXES apiserver - put openworld and defer world.Close() inside goroutine because scope and was closing when calling routine finished AndroidManifext.xml - WRITE ext storage permission needed for writing; go figure /world/world.go - added nil check before closing errored world file because panics on nil.Close() These fixes finally have the api server opening the level and returning data

--- android/apiserver/apiserver.go
+++ android/apiserver/apiserver.go
@@ -1,64 +1,42 @@
 package apiserver
 
 import (
-	"io"
-	"io/ioutil"
-	"net/http"
+	"github.com/midnightfreddie/McpeTool/api"
+	"github.com/midnightfreddie/McpeTool/world"
 )
 
 // func StartApiServer(path string) {
 func StartApiServer() {
-	go func() {
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			// files, err := ioutil.ReadDir(path + "/db")
-			files, err := ioutil.ReadDir(`/storage/emulated/0/games/com.mojang/minecraftWorlds`)
-			if err != nil {
-				go func() {
-					http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-						io.WriteString(w, "<html><head><title>ApiError</title></head><body><h1 style=\"color: red;\">API Error</h1><div>"+err.Error()+"</div></body></html>")
-					})
-					http.ListenAndServe(":8080", nil)
-				}()
-				return
-			}
-			io.WriteString(w, "<html><head><title>HelloWorld</title></head><body><h1 style=\"color: red;\">Hello world!</h1><ul>")
-			for _, file := range files {
-				// fmt.Println(file.Name())
-				io.WriteString(w, "<li>"+string(file.Name())+"</li>")
-			}
-			io.WriteString(w, "</ul></body></html>")
-		})
-		http.ListenAndServe(":8080", nil)
-	}()
-	// path := `/storage/emulated/0/games/com.mojang/minecraftWorldsh4wKANYDAQA\=`
-	// world, err := world.OpenWorld(path)
-	// if err != nil {
-	// 	go func() {
-	// 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 			// files, err := ioutil.ReadDir(path + "/db")
-	// 			files, err := ioutil.ReadDir(`/storage/emulated/0/games/com.mojang/minecraftWorlds`)
-	// 			if err != nil {
-	// 				http.Error(w, err.Error(), 500)
-	// 				return
-	// 			}
-	// 			io.WriteString(w, "<html><head><title>HelloWorld</title></head><body><h1 style=\"color: red;\">Hello world!</h1><ul>")
-	// 			for _, file := range files {
-	// 				// fmt.Println(file.Name())
-	// 				io.WriteString(w, "<li>"+string(file.Name())+"</li>")
-	// 			}
-	// 			io.WriteString(w, err.Error())
-
-	// 			io.WriteString(w, "</ul></body></html>")
-	// 		})
-	// 		http.ListenAndServe(":8080", nil)
-	// 	}()
-	// 	return
-	// }
-	// defer world.Close()
 	// go func() {
-	// 	err = api.Server(&world)
-	// 	if err != nil {
-	// 		return
-	// 	}
+	// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 		// files, err := ioutil.ReadDir(path + "/db")
+	// 		// files, err := ioutil.ReadDir(`/storage/emulated/0/games/com.mojang/minecraftWorlds`)
+	// 		files, err := ioutil.ReadDir(`/storage/emulated/0/games/com.mojang/minecraftWorlds/3`)
+	// 		// files, err := ioutil.ReadDir(`/storage/emulated/0/games/com.mojang/minecraftWorlds/h4wKANYDAQA\=`)
+	// 		if err != nil {
+	// 			panic(err)
+	// 		}
+	// 		io.WriteString(w, "<html><head><title>HelloWorld</title></head><body><h1 style=\"color: red;\">Hello world!</h1><ul>")
+	// 		for _, file := range files {
+	// 			// fmt.Println(file.Name())
+	// 			io.WriteString(w, "<li>"+string(file.Name())+"</li>")
+	// 		}
+	// 		io.WriteString(w, "</ul></body></html>")
+	// 	})
+	// 	http.ListenAndServe(":8080", nil)
 	// }()
+	// path := `/storage/emulated/0/games/com.mojang/minecraftWorlds/h4wKANYDAQA\=`
+	path := `/storage/emulated/0/games/com.mojang/minecraftWorlds/mod`
+
+	go func() {
+		world, err := world.OpenWorld(path)
+		if err != nil {
+			panic(err)
+		}
+		defer world.Close()
+		err = api.Server(&world)
+		if err != nil {
+			panic(err)
+		}
+	}()
 }
