e91a812d4ce7d3d5981d2fc5c93145a4d2754c71
Jim Nelson <jim@midnightfreddie.com>
2018-01-30 05:02:26 -0600

    Fixed leveldat put

--- mcpetool/leveldat.go
+++ mcpetool/leveldat.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"encoding/binary"
 	"encoding/hex"
+	"strconv"
 
 	"github.com/midnightfreddie/McpeTool/world"
 	"github.com/midnightfreddie/nbt2json"
@@ -33,7 +34,7 @@ var levelDatCommand = cli.Command{
 					return cli.NewExitError(err, 1)
 				}
 				defer myWorld.Close()
-				levelDat, err := myWorld.GetLevelDat()
+				levelDat, version, err := myWorld.GetLevelDatNbtAndVersion()
 				if err != nil {
 					return cli.NewExitError(err, 1)
 				}
@@ -41,7 +42,7 @@ var levelDatCommand = cli.Command{
 				case c.String("dump") == "true":
 					outData = []byte(hex.Dump(levelDat))
 				case c.String("yaml") == "true":
-					outData, err = nbt2json.Nbt2Yaml(levelDat, binary.LittleEndian, jsonComment+" | level.dat | Path "+worldPath)
+					outData, err = nbt2json.Nbt2Yaml(levelDat, binary.LittleEndian, jsonComment+" | level.dat version "+string(version)+" | Path "+worldPath)
 					if err != nil {
 						return cli.NewExitError(err, 1)
 					}
@@ -67,6 +68,11 @@ var levelDatCommand = cli.Command{
 			Usage: "Overwrites level.dat with nbt-to-JSON formatted data",
 			Flags: []cli.Flag{
 				pathFlag,
+				cli.StringFlag{
+					Name:  "ver",
+					Value: "6",
+					Usage: "level.dat version for header. Ignored for binary and base64 input.",
+				},
 				inFlag,
 				dumpFlag,
 				yamlFlag,
@@ -76,35 +82,42 @@ var levelDatCommand = cli.Command{
 			Action: func(c *cli.Context) error {
 				var levelDat []byte
 				var err error
+				version, err := strconv.Atoi(c.String("ver"))
+				if err != nil {
+					return cli.NewExitError(err, 1)
+				}
 				inData, err := readInput(inFile)
 				if err != nil {
 					return cli.NewExitError(err, 1)
 				}
+				myWorld, err := world.OpenWorld(worldPath)
+				if err != nil {
+					return cli.NewExitError(err, 1)
+				}
+				defer myWorld.Close()
 				switch {
 				case c.String("yaml") == "true":
-					levelDat, err = nbt2json.Yaml2Nbt(levelDat, binary.LittleEndian)
+					levelDat, err = nbt2json.Yaml2Nbt(inData, binary.LittleEndian)
 					if err != nil {
 						return cli.NewExitError(err, 1)
 					}
+					err = myWorld.PutLevelDatNbtAndVersion(levelDat, int32(version))
 				case c.String("binary") == "true":
 					levelDat = inData
+					err = myWorld.PutLevelDat(levelDat)
 				case c.String("base64") == "true":
 					levelDat, err = base64.StdEncoding.DecodeString(string(inData[:]))
 					if err != nil {
 						return cli.NewExitError(err, 1)
 					}
+					err = myWorld.PutLevelDat(levelDat)
 				default:
-					levelDat, err = nbt2json.Json2Nbt(levelDat, binary.LittleEndian)
+					levelDat, err = nbt2json.Json2Nbt(inData, binary.LittleEndian)
 					if err != nil {
 						return cli.NewExitError(err, 1)
 					}
+					err = myWorld.PutLevelDatNbtAndVersion(levelDat, int32(version))
 				}
-				myWorld, err := world.OpenWorld(worldPath)
-				defer myWorld.Close()
-				if err != nil {
-					return cli.NewExitError(err, 1)
-				}
-				err = myWorld.PutLevelDat(levelDat)
 				if err != nil {
 					return cli.NewExitError(err, 1)
 				}
