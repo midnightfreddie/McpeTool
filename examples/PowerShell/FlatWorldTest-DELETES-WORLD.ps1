de560028359992f638ef545f202be8264ce3130a
Jim Nelson <jim@midnightfreddie.com>
2018-02-02 19:52:43 -0600

    flat world test placing sparse full chunks

--- examples/PowerShell/FlatWorldTest-DELETES-WORLD.ps1
+++ examples/PowerShell/FlatWorldTest-DELETES-WORLD.ps1
@@ -16,13 +16,25 @@
 # $subChunk = New-Object byte[] (1 + 4096 + 2048 + 2048 + 2048)
 # It seems to be okay to leave off sky light and block light; at least generated chunks don't store them
 $subChunk = New-Object byte[] (1 + 4096 + 2048)
-# $subChunk[0] = [byte]1
-$BlockId = [byte]2
-# $BlockId = [byte]57
-# Put a grass block in all the y=0 locations
+# Fill subchunk with stone
+for ($x = 1; $x -lt 4097; $x++) { 
+    $subChunk[$x] = [byte]1
+}
+$TopSubChunk = $subChunk | ForEach-Object { $PSItem }
+$BottomSubChunk = $subChunk | ForEach-Object { $PSItem }
+# Put grass/dirt blocks in the top 3 layers
 for ($x = 0; $x -lt 16; $x++) { 
     for ($z = 0; $z -lt 16; $z++) { 
-        $subChunk[1 + ($x * 16 * 16) + ($z * 16)] = $BlockId
+        $TopSubChunk[1 + ($x * 16 * 16) + ($z * 16) + 12] = [byte]3
+        $TopSubChunk[1 + ($x * 16 * 16) + ($z * 16) + 13] = [byte]3
+        $TopSubChunk[1 + ($x * 16 * 16) + ($z * 16) + 14] = [byte]2
+        $TopSubChunk[1 + ($x * 16 * 16) + ($z * 16) + 15] = [byte]0
+    }    
+}
+# Put bedrock blocks in bottom layer
+for ($x = 0; $x -lt 16; $x++) { 
+    for ($z = 0; $z -lt 16; $z++) { 
+        $BottomSubChunk[1 + ($x * 16 * 16) + ($z * 16)] = [byte]7
     }    
 }
 
@@ -34,6 +46,14 @@ $KeyList.keys | ForEach-Object {
 
 $SubchunkBody = New-Object psobject -Property @{
     base64Data = [System.Convert]::ToBase64String($subChunk)
+} | ConvertTo-Json
+
+$TopSubchunkBody = New-Object psobject -Property @{
+    base64Data = [System.Convert]::ToBase64String($TopSubChunk)
+} | ConvertTo-Json
+
+$BottomSubchunkBody = New-Object psobject -Property @{
+    base64Data = [System.Convert]::ToBase64String($BottomSubChunk)
 } | ConvertTo-Json
 
 $ChunkVersionBody = New-Object psobject -Property @{
@@ -48,8 +68,8 @@ $ChunkVersionBody = New-Object psobject -Property @{
 [int32]$ChunkX = [Math]::Floor($SpawnX / 16)
 [int32]$ChunkZ = [Math]::Floor($SpawnZ / 16)
 
-for ($x = $ChunkX - 5; $x -lt $ChunkX + 5; $x++) { 
-    for ($z = $ChunkZ - 5; $z -lt $ChunkZ + 5; $z++) { 
+for ($x = $ChunkX - 6; $x -lt $ChunkX + 6; $x+=2) { 
+    for ($z = $ChunkZ - 6; $z -lt $ChunkZ + 6; $z+=2) { 
         $HexPrefix = (
             @([System.BitConverter]::GetBytes([int32]$x)) +
             [System.BitConverter]::GetBytes([int32]$z) |
@@ -58,7 +78,10 @@ for ($x = $ChunkX - 5; $x -lt $ChunkX + 5; $x++) {
             }
             ) -join ''
         # $HexPrefix
-        Invoke-RestMethod -Uri "http://127.0.0.1:8080/api/v1/db/${HexPrefix}2f04" -Method Put -Body $SubchunkBody
+        Invoke-RestMethod -Uri "http://127.0.0.1:8080/api/v1/db/${HexPrefix}2f00" -Method Put -Body $BottomSubchunkBody
+        Invoke-RestMethod -Uri "http://127.0.0.1:8080/api/v1/db/${HexPrefix}2f01" -Method Put -Body $SubchunkBody
+        Invoke-RestMethod -Uri "http://127.0.0.1:8080/api/v1/db/${HexPrefix}2f02" -Method Put -Body $SubchunkBody
+        Invoke-RestMethod -Uri "http://127.0.0.1:8080/api/v1/db/${HexPrefix}2f03" -Method Put -Body $TopSubchunkBody
         Invoke-RestMethod -Uri "http://127.0.0.1:8080/api/v1/db/${HexPrefix}76" -Method Put -Body $ChunkVersionBody
         # Meh, for starters just manually put one subchunk:
         # Invoke-RestMethod -Uri "http://127.0.0.1:8080/api/v1/db/05000000010000002f05" -Method Put -Body $Body
