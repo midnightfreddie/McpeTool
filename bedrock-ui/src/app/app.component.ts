c9ede8b90ca0445f4a35196a70c87c1e15041106
Jim Nelson <jim@jimnelson.us>
2018-12-31 02:00:16 -0600

    Implemented key selectors and chunk map need to move chunk map to its own component and route it

--- bedrock-ui/src/app/app.component.ts
+++ bedrock-ui/src/app/app.component.ts
@@ -5,17 +5,35 @@ import { GameDataService } from './game-data.service';
 @Component({
   selector: 'app-root',
   template: `
-    Under Construction
-    <li *ngFor="let foo of gameDataService.stringKeys()">{{ foo }}</li>
+    Under Construction {{ title }}
+    <svg [attr.viewBox]="viewBox()">
+      <g *ngFor="let chunk of chunks()">
+        <title>{{ jsonDump(chunk) }}</title>
+        <rect [attr.x]="chunk.cx" [attr.y]="chunk.cz" width="0.8" height="0.8"></rect>
+      </g>
+    </svg>
     <router-outlet></router-outlet>
   `,
   styles: []
 })
 export class AppComponent implements OnInit {
   title = 'bedrock-ui';
+  chunks = () => this.gameDataService.overworldChunks().map(e => this.gameDataService.chunkKeyInfo(`${e}32`));
+  jsonDump = data => JSON.stringify(data, null, 2);
+  viewBox = () => {
+    let minX = Math.min(...this.chunks().map(e => e['cx']));
+    let minY = Math.min(...this.chunks().map(e => e['cz']));
+    let width = Math.max(...this.chunks().map(e => e['cx'])) - minX;
+    let height = Math.max(...this.chunks().map(e => e['cz'])) - minY;
+    let out = `${minX} ${minY} ${width} ${height}`;
+    // return some default if any values are Infinity or NaN
+    return /^[\d -]+$/.test(out) ? out : "-16 -16 32 32";
+  }
   constructor(private gameDataService: GameDataService) { }
   ngOnInit() {
     this.gameDataService.refreshKeys();
+    let myfoo = '0100000001000000';
+    console.log(this.gameDataService.chunkKeyInfo(`${myfoo}32`));
   }
 
 }
--- bedrock-ui/src/app/game-data.service.ts
+++ bedrock-ui/src/app/game-data.service.ts
@@ -2,19 +2,29 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 const apiRoot = 'http://127.0.0.1:8080/api/v1';
-/*
-const nonChunkKeys = [
-  'mVillages',
-  'AutonomousEntities',
-  'BiomeData',
-  'Overworld',
-  'dimension0',
-  'portals'
-];
-*/
-// const allPrintableAscii = /^[ -~]+$/;
 // Matches strings that are hex strings of all-printable ascii (plus 0x7f, but oh well)
-const allPrintableAscii = /^([2-7][0-9a-fA-f])+$/;
+const allPrintableAscii = /^([2-7][0-9a-f])+$/i;
+// Matches hex strings for ~local_player and those begining with player_
+const playerKeyRegEx = /^(7e6c6f63616c5f706c61796572$|706c617965725f)/i;
+const chunkKeyRegEx = /^(?<cx>[\da-f]{8})(?<cz>[\da-f]{8})(?<dimension>[\da-f]{8})?(?<tag>[\da-f]{2})(?<subchunk>[\da-f]{2})?$/i;
+const chunkTags = {
+  45: 'Data2D',
+  46: 'Data2DLegacy',
+  47: 'SubChunkPrefix',
+  48: 'LegacyTerrain',
+  49: 'BlockEntity',
+  50: 'Entity',
+  51: 'PendingTicks',
+  52: 'BlockExtraData',
+  53: 'BiomeState',
+  54: 'FinalizedState',
+  118: 'Version',
+}
+
+export const xPerChunk = 16;
+export const zPerChunk = 16;
+export const yPerPEChunk = 128;
+export const yPerSubChunk = 16;
 
 @Injectable({
   providedIn: 'root'
@@ -24,30 +34,12 @@ export class GameDataService {
   constructor(private http:HttpClient) { }
 
   keyList: string[] = [];
-/*
-  otherKeys;
-  players = {};
-  chunks = {};
-  knownKeys = {};
-  unKnownKeys = {};
-  entityChunkList = [];
-*/
-/*
-  zeroKeys = () => {
-    this.keyList = [];
-    this.players = {};
-    this.chunks = {};
-    this.knownKeys = {};
-    this.unKnownKeys = {};
-    this.entityChunkList = [];
-  }
-*/
 
   // Fetches key list from API and populates class property
   // TODO: Handle errors
   refreshKeys = () => {
     this.http.get(`${apiRoot}/db/`)
-      .subscribe( data => this.keyList = data["keys"].map(e => e.hexKey));
+      .subscribe( data => this.keyList = data['keys'].map(e => e.hexKey));
   }
 
   // Returns ASCII strings of hex keys that are all printable values
@@ -62,72 +54,52 @@ export class GameDataService {
     return out;
   }
 
-    /* *** Will move this logic to other methods ***
-  // Have to use arrow function notation to preserve 'this' in success function
-  refreshKeysSuccess = (data) => {
-    this.zeroKeys();
-    this.keyList = data.keys.map(e => e.hexKey);
-    console.log(this.keyList);
-    this.keyList.forEach(e => {
-      let chunk: string;
-      // regex to match player string keys TODO: use hex key match instead?
-      if (/^(~local_player|^player_)/.test(e.stringKey)) {
-        this.players[e.stringKey] = e;
-      } else if (nonChunkKeys.includes(e.stringKey)) {
-        this.knownKeys[e.stringKey] = e;
-      // matches 9, 10, 13 or 14 hex digits and captures matches
-      // 1: x, 2: z, 3: (if present) dimension, 4: tag / data type, 5: subchunk
-      } else if (chunk = e.hexKey.match(/^([\da-f]{8})([\da-f]{8})([\da-f]{8})?([\da-f]{2})([\da-f]{2})?$/i)) {
-        // Currently using x, z, and dimension (if present) portion of key to identify the chunk
-        let hexKey = chunk[1] + chunk[2] + (chunk[3] || "");
-        // Create key if not present
-        if (! this.chunks[hexKey]) {
-          // create array so buffer.slice is available
-          let byteArrayKey = new Uint8Array(hexKey.length / 2);
-          // populate the array
-          for (let i = 0; i < hexKey.length; i += 2) {
-            byteArrayKey[i/2] = parseInt("0x" + hexKey.substr(i, 2), 16);
-          }
-          this.chunks[hexKey] = {
-            dimension: chunk[3] ? new Int32Array((new Uint8Array(byteArrayKey)).buffer.slice(8,12))[0] : 0,
-            "x": new Int32Array((new Uint8Array(byteArrayKey)).buffer.slice(0,4))[0],
-            "z": new Int32Array((new Uint8Array(byteArrayKey)).buffer.slice(4,8))[0],
-            subChunks: [],
-            unknown: []
-          }
-        }
-        switch (chunk[4]) {
-          case '2d':
-            this.chunks[hexKey]['data2d'] = e.url;
-            break;
-          case '2f':
-            this.chunks[hexKey].subChunks.push(e.hexKey);
-            break;
-            case '31':
-            this.chunks[hexKey]['block-entities'] = e.hexKey;
-            break;
-            case '32':
-            this.chunks[hexKey]['entities'] = e.hexKey;
-            this.entityChunkList.push(e.hexKey);
-            break;
-          case '36':
-            this.chunks[hexKey]['0x36'] = e.hexKey;
-            break;
-          case '76':
-            this.chunks[hexKey]['version'] = e.hexKey;
-            break;
-          default:
-            this.chunks[hexKey].unknown.push(e.hexKey);
-        }
-      } else {
-        this.unKnownKeys[e.hexKey] = e.hexKey;
+  playerKeys = () => this.keyList.filter(e => playerKeyRegEx.test(e)).map(e => this.asciiHexStringToString(e));
+
+  // Returns keys that the author knows of but hasn't handled yet
+  knownKeys = () => this.keyList.filter(e => [
+    'mVillages',
+    'AutonomousEntities',
+    'BiomeData',
+    'Overworld',
+    'dimension0',
+    'portals'
+  ].includes(this.asciiHexStringToString(e))).map(e => this.asciiHexStringToString(e));
+
+  // Returns keys for chunks. The chunkKeyRegEx matches some of the string keys, so printable ASCII keys
+  chunkKeys = () => this.keyList.filter(e => chunkKeyRegEx.test(e) && !allPrintableAscii.test(e));
+
+  // Given a chunk hex key, returns derivable info
+  // When passing the output of overworldChunks(), have to add a fake tag for now, e.g. '32'
+  chunkKeyInfo = (hexKey: string) => {
+    const chunk = hexKey.match(chunkKeyRegEx);
+    let out = {};
+    out['dimension'] = (chunk['groups'] || {})['dimension'] ? parseInt(chunk['groups']['dimension'], 16) : 0;
+    for (let e of ['cx', 'cz', 'tag', 'subchunk']) {
+      if ((chunk['groups'] || {})[e]) {
+        out[e] = this.hexStringToInt(chunk['groups'][e]);
       }
-    });
+    }
+    return out;
   }
-    */
-/*
-  httpFail = (response) => {
-    console.error('http failure', response.status, response.statustext, response.data);
+
+  // Return deduplicated list of 16-length hex strings for overworld chunks
+  overworldChunks = () => this.chunkKeys()
+    .filter(e => {
+      const chunkInfo = this.chunkKeyInfo(e);
+      return chunkInfo['dimension'] == 0;
+    })
+    .map(e => e.substring(0,16))
+    .filter((e, i, arr) => arr.indexOf(e) === i);
+
+  private hexStringToInt = (hexString: string): number  => {
+    let msbString: string = "";
+    for (let i=hexString.length - 2; i >= 0 ; i -=2) {
+      msbString += hexString.substring(i, i+2);
+    }
+    let out = parseInt(msbString, 16);
+    // Hard coding for signed 32-bit integers; effectively shorter ints unsigned
+    return out > 0x80000000 ? out - 0x100000000 : out;
+
   }
-*/
 }
