6b85a915eaf0074f6d88818fccbc2da591fbed1d
Jim Nelson <jim@jimnelson.us>
2019-01-02 00:33:40 -0600

    added key and base64key fields

--- graphql/graphql.go
+++ graphql/graphql.go
@@ -13,23 +13,28 @@ import (
 
 // DbObject is used for dbKeys results
 type DbObject struct {
-	key        []byte
+	Key        []byte `json:"key,omitempty"`
 	data       []byte
 	StringKey  string `json:"stringKey,omitempty"`
 	HexKey     string `json:"hexKey,omitempty"`
 	Base64Data string `json:"base64Data,omitempty"`
+	Base64Key  string `json:"base64Key,omitempty"`
 }
 
 // Fill is used to convert the raw byte arrays to JSON-friendly data before returning to client
 func (o *DbObject) Fill() {
-	o.StringKey, o.HexKey = ConvertKey(o.key)
+	o.StringKey, o.HexKey = ConvertKey(o.Key)
 	o.Base64Data = base64.StdEncoding.EncodeToString(o.data)
+	o.Base64Key = base64.StdEncoding.EncodeToString(o.Key)
 }
 
 var dbObjectType = graphql.NewObject(
 	graphql.ObjectConfig{
 		Name: "DbObject",
 		Fields: graphql.Fields{
+			"key": &graphql.Field{
+				Type: graphql.NewList(graphql.Int),
+			},
 			"hexKey": &graphql.Field{
 				Type: graphql.String,
 			},
@@ -37,6 +42,9 @@ var dbObjectType = graphql.NewObject(
 				Type: graphql.String,
 			},
 			"base64Data": &graphql.Field{
+				Type: graphql.String,
+			},
+			"base64Key": &graphql.Field{
 				Type: graphql.String,
 			},
 		},
@@ -105,7 +113,7 @@ func Server(world *world.World, bindAddress, bindPort string) error {
 					var outData []DbObject
 					for i := range keyList {
 						thisKey := new(DbObject)
-						thisKey.key = keyList[i]
+						thisKey.Key = keyList[i]
 						thisKey.Fill()
 						if ok && stringKeysOnly {
 							if thisKey.StringKey != "" {
